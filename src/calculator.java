f2cf6a36f30fada11c2983ac8c8a97792ed48b6f
prana.wijaya <prana.wijaya@metraplasa.co.id>
2020-03-31 14:21:52 +0700

    Initial commit

---
+++ src/calculator.java
@@ -0,0 +1,60 @@
+import java.util.Scanner;
+import java.lang.*;
+
+public class calculator {
+    public static void main(String[] args){
+        System.out.println("Calculator");
+
+
+        Scanner scan = new Scanner(System.in);
+
+
+        System.out.println("Input Number: ");
+        int firstValue = scan.nextInt();
+
+        String[] menu = {"addition", "Subtraction", "Multipication", "Division", "Modulo", "Clear Total", "Exit"};
+
+        for (int i = 0; i < menu.length ; i++) {
+            System.out.println(i+1 +". " + menu[i]);
+        }
+
+        System.out.println("Choose Menu");
+        int chooseMenu = scan.nextInt();
+
+       System.out.println("Input Second Number");
+       int secondValue = scan.nextInt();
+
+       float total = 0;
+
+       switch (chooseMenu){
+           case 1 :
+               total = firstValue + secondValue;
+               break;
+           case 2:
+               total = firstValue - secondValue;
+               break;
+           case 3:
+               total = firstValue * secondValue;
+               break;
+           case 4:
+               total = firstValue / secondValue;
+               break;
+           case 5:
+               total = firstValue % secondValue;
+               break;
+           case 6:
+               total = 0;
+               break;
+           case 7:
+               System.out.println("exit... bye");
+               System.exit(0);
+               break;
+           default:
+               System.out.println("input not found");
+               break;
+       }
+
+        System.out.println("=====================");
+        System.out.println("Total = " + total);
+    }
+}
